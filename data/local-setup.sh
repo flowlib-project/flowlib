3619f744f9e3a4fb3bc9352c918bb58037448706
David Kegley <kegs@b23.io>
2019-04-16 14:57:59 -0400

    update local setup script and fix zk liveness check

--- data/local-setup.sh
+++ data/local-setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+datadir=$(dirname "$0")
 mkdir -p /tmp/docker
-mkdir -p $(dirname "$0")/zookeeper/{pv1,pv2,pv3}
-mkdir -p $(dirname "$0")/nifi/{pv1,pv2,pv3}/{flowfile_repository,database_repository,content_repository,provenance_repository}
-(sudo rm -f /tmp/docker/nifi; cd $(dirname "$0")/nifi && sudo ln -s $(pwd) /tmp/docker/nifi)
-(sudo rm -f /tmp/docker/zookeeper; cd $(dirname "$0")/zookeeper && sudo ln -s $(pwd) /tmp/docker/zookeeper)
+rm -r $datadir/zookeeper && mkdir -p $datadir/zookeeper/{pv1,pv2,pv3}
+rm -r $datadir/nifi && mkdir -p $datadir/nifi/{pv1,pv2,pv3}/{flowfile_repository,database_repository,content_repository,provenance_repository}
+(sudo rm -f /tmp/docker/nifi; cd $datadir/nifi && sudo ln -s $(pwd) /tmp/docker/nifi)
+(sudo rm -f /tmp/docker/zookeeper; cd $datadir/zookeeper && sudo ln -s $(pwd) /tmp/docker/zookeeper)
