c7ec4508fb11dc9e53213eb7cbf32bd0d70b1da5
David Kegley <kegs@b23.io>
2019-04-19 14:38:53 -0400

    update local volume setup script

--- data/local-setup.sh
+++ data/local-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 datadir=$(dirname "$0")
 mkdir -p /tmp/docker
-rm -r $datadir/zookeeper && mkdir -p $datadir/zookeeper/{pv1,pv2,pv3}
-rm -r $datadir/nifi && mkdir -p $datadir/nifi/{pv1,pv2,pv3}/{flowfile_repository,database_repository,content_repository,provenance_repository}
+rm -rf $datadir/zookeeper && mkdir -p $datadir/zookeeper/{pv1,pv2,pv3}
+rm -rf $datadir/nifi && mkdir -p $datadir/nifi/{pv1,pv2,pv3}/{flowfile_repository,database_repository,content_repository,provenance_repository}
 (sudo rm -f /tmp/docker/nifi; cd $datadir/nifi && sudo ln -s $(pwd) /tmp/docker/nifi)
 (sudo rm -f /tmp/docker/zookeeper; cd $datadir/zookeeper && sudo ln -s $(pwd) /tmp/docker/zookeeper)
