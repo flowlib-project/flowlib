6d46ef472c1a736862a75450cc71b93a325ce339
David Kegley <kegs@b23.io>
2019-05-07 12:28:51 -0400

    fix release script

--- release.sh
+++ release.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash -e
-DIRTY=""
-[[ -z $(git status -s) ]] || DIRTY="dirty"
-
 TAG=$(python ./setup.py --version)
 if echo $TAG | grep 'dev'; then
   TAG="latest"
@@ -22,7 +19,7 @@ DIST=$(ls ./dist)
 # Login to ECR
 $(aws ecr get-login --no-include-email --region us-east-1)
 
-docker build --build-arg FLOWLIB_DIST=$DIST --no-cache $(dirname $0) -t b23-flowlib:$TAG
+docker build $(dirname $0) --build-arg FLOWLIB_DIST=$DIST --no-cache -t b23-flowlib:$TAG
 docker tag b23-flowlib:$TAG 883886641571.dkr.ecr.us-east-1.amazonaws.com/b23-flowlib:$TAG
 docker push 883886641571.dkr.ecr.us-east-1.amazonaws.com/b23-flowlib:$TAG
 
