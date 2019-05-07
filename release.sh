f9ec646429a59e01be0bae9fe69d0804017fc10c
David Kegley <kegs@b23.io>
2019-05-07 00:32:12 -0400

    add dev requiremnets file

--- release.sh
+++ release.sh
@@ -12,6 +12,6 @@ fi
 # Login to ECR
 $(aws ecr get-login --no-include-email --region us-east-1)
 
-docker build --no-cache $(dirname $0) -f $(dirname $0)/flowlib.dockerfile -t b23-flowlib:$TAG
+docker build --no-cache $(dirname $0) -t b23-flowlib:$TAG
 docker tag b23-flowlib:$TAG 883886641571.dkr.ecr.us-east-1.amazonaws.com/b23-flowlib:$TAG
 docker push 883886641571.dkr.ecr.us-east-1.amazonaws.com/b23-flowlib:$TAG
