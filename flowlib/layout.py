c6558d719408cb395033c69bedca08a7d27a00bd
mark <mark@b23.io>
2019-08-27 09:41:59 -0400

    Add future support for modular layout types

--- flowlib/layout.py
+++ flowlib/layout.py
@@ -2,13 +2,17 @@ import math
 import networkx as nx
 import random
 
+from flowlib.model import FlowLibException
+
 TOP_LEVEL_PG_LOCATION = (300, 100)
 
-def generate_layout(elements):
+def generate_layout(elements, layout_type="spring"):
     """
     Generate a set of x,y positions given a set of elements and connections
     :param elements: The elements to deploy
     :type elements: list(model.FlowElement)
+    :param layout_type: The type of layout (e.g., spring, planar)
+    :type layout_type: str
     """
     G = nx.Graph()  # Initialize an empty graph
 
@@ -26,8 +30,14 @@ def generate_layout(elements):
 
     scale = 100 * len(G.nodes)  # Make the canvas size scale with the total number of elements
 
-    # Run the force directed layout algorithm
-    positions = nx.spring_layout(G, scale=scale, center=TOP_LEVEL_PG_LOCATION, pos=initial_positions, k=100)
+    if layout_type == "spring":
+        positions = nx.spring_layout(G, scale=scale, center=TOP_LEVEL_PG_LOCATION, pos=initial_positions, k=100)
+    elif layout_type == "planar":
+        positions = nx.planar_layout(G, scale=scale, center=TOP_LEVEL_PG_LOCATION)
+    elif layout_type == "spectral":
+        positions = nx.spectral_layout(G, scale=scale, center=TOP_LEVEL_PG_LOCATION)
+    else:
+        raise FlowLibException("Unsupported Graph Laout Type: {}".format(type))
 
     # Convert [x,y] to (x,y) since nipyapi expects a tuple
     for name, pos in positions.items():
