65fce98c5f878eb51faffbed1f69f5fef073d74b
mark <mark@b23.io>
2019-09-25 15:01:33 -0400

    Use valid characters for package name

--- flowlib-metrics/flowlib.metrics.nifi/nifi-flowlib.metrics.nifi-processors/src/main/java/io/b23/processors/flowlib/metrics/nifi/MyProcessor.java
+++ flowlib-metrics/flowlib.metrics.nifi/nifi-flowlib.metrics.nifi-processors/src/main/java/io/b23/processors/flowlib/metrics/nifi/MyProcessor.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.b23.processors.flowlib-metrics-nifi;
+package io.b23.processors.flowlib.metrics.nifi;
 
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.flowfile.FlowFile;
--- flowlib-metrics/flowlib.metrics.nifi/nifi-flowlib.metrics.nifi-processors/src/test/java/io/b23/processors/flowlib/metrics/nifi/MyProcessorTest.java
+++ flowlib-metrics/flowlib.metrics.nifi/nifi-flowlib.metrics.nifi-processors/src/test/java/io/b23/processors/flowlib/metrics/nifi/MyProcessorTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.b23.processors.flowlib-metrics-nifi;
+package io.b23.processors.flowlib.metrics.nifi;
 
 import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
