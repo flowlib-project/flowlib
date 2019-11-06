18859d64c8ff4ee5e868cd2171f590daf3f0322b
David Kegley <kegs@b23.io>
2019-11-06 11:46:24 -0500

    update integration tests to deploy from deployment.json

--- tests/integration/itest_scaffold_deploy.py
+++ tests/integration/itest_scaffold_deploy.py
@@ -7,7 +7,8 @@ import nipyapi.nifi.apis
 from nipyapi.nifi import ProcessGroupEntity, ProcessorEntity
 
 import flowlib.api
-from flowlib.exceptions import FlowLibException
+import flowlib.nifi.rest
+from flowlib.exceptions import FlowLibException, FlowNotFoundException
 from flowlib.model.config import FlowLibConfig
 from flowlib.nifi.rest import wait_for_nifi_api
 from flowlib.nifi.state import ZookeeperClient
@@ -17,10 +18,26 @@ from tests.integration import ITestBase
 
 class ITestScaffoldDeploy(ITestBase):
 
+    def tearDown(self):
+        with open('.flowlib.yml', 'r') as f:
+            config = FlowLibConfig.new_from_file(f)
+        config.nifi_endpoint = self.nifi
+        flowlib.nifi.rest._force_cleanup_reporting_tasks()
+        try:
+            flow_pg = flowlib.nifi.rest._find_flow_by_name('pdf-processor-demo-flow')
+            flowlib.nifi.rest._remove_flow(flow_pg.id, force=True)
+        except FlowNotFoundException:
+            pass
+
     def runTest(self):
+        initial_state = {'asdf': 'This is state'}
+        deployment_state = {'test': 'This state will overwrite initial state'}
+
         self._test_configure_flow_controller()
         self._test_deploy_flow()
-        self._test_redeploy_flow()
+        self._test_redeploy_flow(initial_state)
+        self._test_export_flow(initial_state)
+        self._test_deploy_from_deployment_json(deployment_state)
 
     def _test_configure_flow_controller(self):
         with open('.flowlib.yml', 'r') as f:
@@ -38,9 +55,11 @@ class ITestScaffoldDeploy(ITestBase):
         config.flow_yaml = 'flow.yaml'
 
         flowlib.api.deploy_flow(config)
-        self.assertIsInstance(nipyapi.canvas.get_process_group('pdf-processor-demo-flow'), ProcessGroupEntity)
+        flow_pg = nipyapi.canvas.get_process_group('pdf-processor-demo-flow')
+        ITestScaffoldDeploy.flow_pg_id = flow_pg.id
+        self.assertIsInstance(flow_pg, ProcessGroupEntity)
 
-    def _test_redeploy_flow(self):
+    def _test_redeploy_flow(self, state):
         with open('.flowlib.yml', 'r') as f:
             config = FlowLibConfig.new_from_file(f)
         config.nifi_endpoint = self.nifi
@@ -49,7 +68,7 @@ class ITestScaffoldDeploy(ITestBase):
 
         listS3 = nipyapi.canvas.get_processor('list-s3')
         self.assertIsInstance(listS3, ProcessorEntity)
-        state = {'asdf': 'This is state'}
+
         zk = ZookeeperClient(config.zookeeper_connection)
         zk.set_processor_state(listS3.id, state)
 
@@ -57,4 +76,43 @@ class ITestScaffoldDeploy(ITestBase):
         flowlib.api.deploy_flow(config)
         listS3 = nipyapi.canvas.get_processor('list-s3')
         self.assertIsInstance(listS3, ProcessorEntity)
+        self.assertEqual(state, zk.get_processor_state(listS3.id))
+
+    def _test_export_flow(self, state):
+        with open('.flowlib.yml', 'r') as f:
+            config = FlowLibConfig.new_from_file(f)
+        config.nifi_endpoint = self.nifi
+        config.export = 'pdf-processor-demo-flow'
+        with open('deployment.json', 'w') as f:
+            flowlib.api.export_flow(config, fp=f)
+
+        with open('deployment.json', 'r') as f:
+            flow, deployment = flowlib.api.new_flow_from_deployment(f)
+
+        lists3 = deployment.get_component('s3-list-fetch-with-retry')
+        self.assertEqual(list(lists3.stateful_processors.values())[0]['state'], state)
+
+    def _test_deploy_from_deployment_json(self, state):
+        with open('.flowlib.yml', 'r') as f:
+            config = FlowLibConfig.new_from_file(f)
+        config.nifi_endpoint = self.nifi
+        config.zookeeper_connection = self.zookeeper
+
+        with open('./deployment.json', 'r') as f:
+            flow, deployment = flowlib.api.new_flow_from_deployment(f)
+
+        # overwrite the deployment's state
+        lists3 = deployment.get_component('s3-list-fetch-with-retry')
+        list(lists3.stateful_processors.values())[0]['state'] = state
+
+        self.assertRaisesRegex(FlowLibException, "^A flow with that name already exists, use the --force option to overwrite it$",
+            flowlib.nifi.rest.deploy_flow, flow, config, deployment=deployment, force=False)
+
+        # overwrite the currently deployed flow with the provided deployment
+        flowlib.nifi.rest.deploy_flow(flow, config, deployment=deployment, force=True)
+
+        # make sure that the newly deployed flow has the same state that was set explicitly in the deployment,
+        # not the state that it had initially
+        listS3 = nipyapi.canvas.get_processor('list-s3')
+        zk = ZookeeperClient(config.zookeeper_connection)
         self.assertEqual(state, zk.get_processor_state(listS3.id))
