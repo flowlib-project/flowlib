6b1d41e363d33e76c3e3020e77ad3c2af0296900
Virginia Allen <vallen@Virginias-MacBook-Pro.local>
2022-01-21 23:25:15 -0500

    FLOW-4 - Use JSON output from NiFi ToolKit instead of strings

--- flowlib/new/registry.py
+++ flowlib/new/registry.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
-from flowlib.new.util import parse_output
+from flowlib.new.util import call_cmd
 
 def list_flows(config, bucket_name):
-    all_buckets = parse_output(config.registry_endpoint, "registry list-buckets")
-    if bucket_name == "all":
-        for (k,v) in all_buckets.items():
-            print("Bucket: {}".format(k))
-            __display_flows(config, v)
-    else:
-        bucket_id = {v for (k,v) in all_buckets.items() if k in bucket_name}
-        print("Bucket: {}".format(bucket_name))
-        __display_flows(config, bucket_id.pop())
+    all_buckets = call_cmd(config.registry_endpoint, "registry list-buckets")
+    for bucket in all_buckets:
+        if bucket_name == "all" or bucket_name == bucket['name']:
+            print("Bucket: {}".format(bucket['name']))
+            __display_flows(config, bucket['identifier'])
+
 
 def __display_flows(config, bucket_id):
-    flows = parse_output(config.registry_endpoint, "registry list-flows --bucketIdentifier {id}".format(id=bucket_id))
+    flows = call_cmd(config.registry_endpoint, "registry list-flows --bucketIdentifier {id}".format(id=bucket_id))
     if flows:
-        i = 1;
-        for key in flows.keys():
-            print('\t{}. {}'.format(i, key))
+        i = 1
+        for key in flows:
+            print('\t{}. {}\tVersion {}'.format(i, key['name'], key['versionCount']))
             i += 1
     else:
         print("\t! No flows available")
